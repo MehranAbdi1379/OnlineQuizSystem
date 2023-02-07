465a61a35eb5e3eff69e1ced00792e70d5460208
Mehran Abdi <mehranabdi13793@gmail.com>
2023-02-07 12:14:14 +0330

    Add Service Layer

---
+++ OnlineQuizSystem.Service.Contracts/IEmployeeServcie.cs
@@ -0,0 +1,9 @@
+﻿using OnlineQuizSystem.Domain;
+using OnlineQuizSystem.Service.Contracts.Dto;
+
+namespace OnlineQuizSystem.Service.Contracts;
+
+public interface IEmployeeService
+{
+    Employee CreateEmployee(CreateEmployeeDto dto);
+}
---
+++ OnlineQuizSystem.Service/EmployeeService.cs
@@ -0,0 +1,35 @@
+﻿using OnlineQuizSystem.Domain;
+using OnlineQuizSystem.Domain.Repositories;
+using OnlineQuizSystem.Service.Contracts;
+using OnlineQuizSystem.Service.Contracts.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineQuizSystem.Service;
+
+public class EmployeeService : IEmployeeService
+{
+    private readonly IEmployeeRepository repository;
+
+    public EmployeeService(IEmployeeRepository repository)
+    {
+        this.repository = repository;
+    }
+    
+    public Employee CreateEmployee(CreateEmployeeDto dto)
+    {
+        var employee = new Employee(
+                dto.FirstName,
+                dto.LastName,
+                dto.NationalCode,
+                dto.BirthDate,
+                repository
+            );
+
+        repository.Create(employee);
+        return employee;
+    }
+}
