f92ea4411052e95def54e76ce04d117b38e87100
Mehran Abdi <mehranabdi13793@gmail.com>
2023-02-07 12:13:08 +0330

    Add FrameWork

---
+++ Framework.Core/Repository/IBaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Core.Repository
+{
+    public interface IBaseRepository <TEntity>
+        where TEntity : BaseEntity
+    {
+        void Create(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
---
+++ Framework.Repository/BaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using Framework.Core;
+using Framework.Core.Repository;
+
+namespace Framework.Repository;
+
+public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : BaseEntity
+{
+    public void Create(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+}
